0c2a0c034b0988a7898eb5895aa8d3a1b18aec0d
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-10-14 21:22:53 -0700

    Engagement module and basic config g * Modularize the mod and allow disabling each module (see `data/config/settings.json`). * Add engagement module - faction rep adjustment based on combat activity.

---
+++ src/main/java/starpocalypse/EngagementListener.java
@@ -0,0 +1,102 @@
+package starpocalypse;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.BaseCampaignEventListener;
+import com.fs.starfarer.api.campaign.FactionAPI;
+import com.fs.starfarer.api.combat.EngagementResultAPI;
+import com.fs.starfarer.api.util.Misc;
+
+import org.apache.log4j.Level;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class EngagementListener extends BaseCampaignEventListener {
+
+    // At most increases rep by 0.01 when faction hates the one you've beaten
+    // (beaten reputation at adjusted faction is -1.0). Negative sign to give
+    // a positive increase to the player.
+    public static float ADJUSTMENT_FACTOR = -100f;
+
+    // Commissioned faction relationship changes faster. Mind your manners.
+    public static float COMMISSIONED_MULT = 3f;
+
+    // Minimal absolute adjustment to consider.
+    public static float MIN_ADJUSTMENT = 0.001f;
+
+    // Never adjust these factions.
+    public static String BLACKLIST[] = { "derelict", "knights_of_ludd", "lions_guard", "mercenary", "neutral", "omega",
+            "player", "poor", "remnant", "scavengers", "sleeper" };
+
+    public EngagementListener() {
+        super(false);
+        Global.getSector().addTransientListener(this);
+        // Change to DEBUG for verbose logging
+        log.setLevel(Level.DEBUG);
+    }
+
+    @Override
+    public void reportPlayerEngagement(EngagementResultAPI result) {
+        if (!result.didPlayerWin()) {
+            log.debug("Skipping since player did not win");
+            return;
+        }
+        FactionAPI beatenFaction = result.getLoserResult().getFleet().getFaction();
+        log.debug("Trying to adjust rep due to beating " + beatenFaction.getId());
+        adjustReputation(beatenFaction);
+    }
+
+    private void adjustReputation(FactionAPI beatenFaction) {
+        for (FactionAPI consideredFaction : Global.getSector().getAllFactions()) {
+            if (!canAdjust(consideredFaction, beatenFaction)) {
+                continue;
+            }
+            adjustReputation(beatenFaction, consideredFaction);
+        }
+    }
+
+    private void adjustReputation(FactionAPI beatenFaction, FactionAPI consideredFaction) {
+        float delta = getAdjustmentDelta(consideredFaction, beatenFaction);
+        if (Math.abs(delta) < MIN_ADJUSTMENT) {
+            log.debug("Skipping faction " + consideredFaction.getId() + " due to small rep change");
+            return;
+        }
+        FactionAPI playerFaction = Global.getSector().getPlayerFaction();
+        log.info("Adjusting faction " + consideredFaction.getId() + " rep by " + String.valueOf(delta));
+        playerFaction.adjustRelationship(consideredFaction.getId(), delta);
+    }
+
+    private boolean canAdjust(FactionAPI faction, FactionAPI beatenFaction) {
+        if (isBlacklisted(faction)) {
+            log.debug("Skipping blacklisted faction " + faction.getId());
+            return false;
+        }
+        if (isFaction(faction, beatenFaction)) {
+            log.debug("Skipping involved faction " + faction.getId());
+            return false;
+        }
+        return true;
+    }
+
+    private float getAdjustmentDelta(FactionAPI consideredFaction, FactionAPI beatenFaction) {
+        float delta = consideredFaction.getRelationship(beatenFaction.getId()) / ADJUSTMENT_FACTOR;
+        FactionAPI commissionedFaction = Misc.getCommissionFaction();
+        if (isFaction(consideredFaction, commissionedFaction)) {
+            log.debug("Increasing rep change for commissioned faction " + consideredFaction.getId());
+            delta *= COMMISSIONED_MULT;
+        }
+        return delta;
+    }
+
+    private boolean isBlacklisted(FactionAPI faction) {
+        for (String blacklistedFactionId : BLACKLIST) {
+            if (blacklistedFactionId.equals(faction.getId())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isFaction(FactionAPI factionA, FactionAPI factionB) {
+        return factionA.equals(factionB);
+    }
+}
--- src/main/java/starpocalypse/StarpocalypseMod.java
+++ src/main/java/starpocalypse/StarpocalypseMod.java
@@ -1,7 +1,11 @@
 package starpocalypse;
 
 import com.fs.starfarer.api.BaseModPlugin;
+import com.fs.starfarer.api.Global;
 
+import lombok.extern.log4j.Log4j;
+
+@Log4j
 public class StarpocalypseMod extends BaseModPlugin {
 
     @Override
@@ -11,7 +15,29 @@ public class StarpocalypseMod extends BaseModPlugin {
 
     @Override
     public void onGameLoad(boolean newGame) {
-        new IndustryChanges();
-        new SubmarketChanges();
+        initEngagementModule();
+        initIndustryModule();
+        initSubmarketModule();
+    }
+
+    private void initEngagementModule() {
+        if (Global.getSettings().getBoolean("starpocalypseEngagementModule")) {
+            log.info("Enabling engagement module");
+            new EngagementListener();
+        }
+    }
+
+    private void initIndustryModule() {
+        if (Global.getSettings().getBoolean("starpocalypseIndustryModule")) {
+            log.info("Enabling industry module");
+            new IndustryChanges();
+        }
+    }
+
+    private void initSubmarketModule() {
+        if (Global.getSettings().getBoolean("starpocalypseSubmarketModule")) {
+            log.info("Enabling submarket module");
+            new SubmarketChanges();
+        }
     }
 }
