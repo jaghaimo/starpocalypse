e269098ff6e284943c956b1e0ff58ea9bb6aec96
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-12-16 14:09:52 +0000

    log module init

--- src/main/java/starpocalypse/PlayerModule.java
+++ src/main/java/starpocalypse/PlayerModule.java
@@ -3,12 +3,15 @@ package starpocalypse;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import java.util.List;
+import lombok.extern.log4j.Log4j;
 import starpocalypse.submarket.ShipDamager;
 
+@Log4j
 public class PlayerModule extends ShipDamager {
 
     public static void init(boolean isEnabled) {
         if (isEnabled) {
+            log.info("Enabling player module");
             PlayerModule module = new PlayerModule();
             List<FleetMemberAPI> members = Global.getSector().getPlayerFleet().getFleetData().getMembersListCopy();
             module.damageShips(members);
