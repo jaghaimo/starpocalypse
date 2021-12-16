f4e36ed04d80f97ba90c162b19a6a78da2f1e4d0
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2021-12-16 17:02:22 +0000

    consistency

--- src/main/java/starpocalypse/SubmarketModule.java
+++ src/main/java/starpocalypse/SubmarketModule.java
@@ -20,10 +20,11 @@ public class SubmarketModule {
 
     public static void init() {
         boolean hasSubmarket = Global.getSettings().getBoolean("starpocalypseSubmarketModule");
-        if (hasSubmarket) {
-            log.info("Enabling submarket module");
-            new SubmarketListener(changers);
+        if (!hasSubmarket) {
+            return;
         }
+        log.info("Enabling submarket module");
+        new SubmarketListener(changers);
         boolean hasShyBlackMarket = Global.getSettings().getBoolean("starpocalypseSubmarketModuleShyBlackMarket");
         if (hasShyBlackMarket) {
             log.info("Enabling shy black market component");
