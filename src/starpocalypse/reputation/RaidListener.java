26bb5ceaa6e08257c74975c0d9d2e34c19194511
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-01-07 14:03:23 +0000

    add raiding

--- src/starpocalypse/reputation/RaidListener.java
+++ src/starpocalypse/reputation/RaidListener.java
@@ -1,13 +1,20 @@
 package starpocalypse.reputation;
 
+import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CargoAPI;
+import com.fs.starfarer.api.campaign.CargoStackAPI;
+import com.fs.starfarer.api.campaign.FactionAPI;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.RepLevel;
 import com.fs.starfarer.api.campaign.econ.Industry;
 import com.fs.starfarer.api.campaign.econ.MarketAPI;
 import com.fs.starfarer.api.campaign.listeners.ColonyPlayerHostileActListener;
 import com.fs.starfarer.api.impl.campaign.rulecmd.salvage.MarketCMD.TempData;
+import starpocalypse.config.SimpleSet;
 
 public class RaidListener implements ColonyPlayerHostileActListener {
+
+    private final SimpleSet protectedItems = new SimpleSet("item", "raidProtectorItem");
 
     @Override
     public void reportRaidForValuablesFinishedBeforeCargoShown(
@@ -17,6 +24,13 @@ public class RaidListener implements ColonyPlayerHostileActListener {
         CargoAPI cargo
     ) {
         CargoAPI raidLoot = actionData.raidLoot;
+        for (CargoStackAPI stack : raidLoot.getStacksCopy()) {
+            String item = stack.getDisplayName();
+            if (protectedItems.has(item)) {
+                setAtWar(market);
+                return;
+            }
+        }
     }
 
     @Override
@@ -36,4 +50,9 @@ public class RaidListener implements ColonyPlayerHostileActListener {
         MarketAPI market,
         TempData actionData
     ) {}
+
+    private void setAtWar(MarketAPI market) {
+        FactionAPI faction = market.getFaction();
+        Global.getSector().getPlayerFaction().setRelationship(faction.getId(), RepLevel.VENGEFUL);
+    }
 }
