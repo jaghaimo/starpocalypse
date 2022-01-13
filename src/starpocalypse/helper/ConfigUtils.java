76ea1388b732b9c939b33a83adff79f2c2ccbf93
Jaghaimo <1764586+jaghaimo@users.noreply.github.com>
2022-01-13 22:02:09 +0000

    stuff

--- src/starpocalypse/helper/ConfigUtils.java
+++ src/starpocalypse/helper/ConfigUtils.java
@@ -3,18 +3,39 @@ package starpocalypse.helper;
 import com.fs.starfarer.api.Global;
 import lombok.Getter;
 import org.json.JSONObject;
+import starpocalypse.config.SimpleMap;
 import starpocalypse.config.SimpleSet;
 
 public class ConfigUtils {
 
     @Getter
-    private static int minDmods;
+    private static int minDmods = 2;
 
     @Getter
-    private static int maxDmods;
+    private static int maxDmods = 4;
 
     @Getter
-    private static boolean shyBlackMarket;
+    private static boolean legacyMilitaryRegulations = false;
+
+    @Getter
+    private static boolean shyBlackMarket = false;
+
+    @Getter
+    private static final SimpleMap regulatedStabilityItem = new SimpleMap(
+        "stability",
+        "item",
+        "militaryRegulationStability.csv"
+    );
+
+    @Getter
+    private static final SimpleMap regulatedStabilityShip = new SimpleMap(
+        "stability",
+        "ship",
+        "militaryRegulationStability.csv"
+    );
+
+    @Getter
+    private static final SimpleSet regulatedLegal = new SimpleSet("faction", "militaryRegulationLegal.csv");
 
     @Getter
     private static final SimpleSet regulatedFaction = new SimpleSet("faction", "militaryRegulationFaction.csv");
@@ -28,6 +49,7 @@ public class ConfigUtils {
     public static void init(JSONObject settings) {
         minDmods = clamp(settings.optInt("minimumDmods", 2), 1, 5);
         maxDmods = clamp(settings.optInt("maximumDmods", 4), minDmods, 5);
+        legacyMilitaryRegulations = settings.optBoolean("legacyMilitaryRegulations", false);
         shyBlackMarket = settings.optBoolean("shyBlackMarket", false);
         if (settings.optBoolean("transparentBlackMarket", true)) {
             float mult = (float) settings.optDouble("transparentBlackMarketMult", 0.5);
--- src/starpocalypse/submarket/RegulatedOpenMarket.java
+++ src/starpocalypse/submarket/RegulatedOpenMarket.java
@@ -1,11 +1,19 @@
 package starpocalypse.submarket;
 
+import com.fs.starfarer.api.campaign.CargoAPI;
 import com.fs.starfarer.api.campaign.CargoStackAPI;
+import com.fs.starfarer.api.campaign.FleetDataAPI;
 import com.fs.starfarer.api.campaign.econ.SubmarketAPI;
+import com.fs.starfarer.api.combat.ShipHullSpecAPI;
+import com.fs.starfarer.api.combat.ShipHullSpecAPI.ShipTypeHints;
+import com.fs.starfarer.api.combat.ShipVariantAPI;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.impl.campaign.ids.HullMods;
 import com.fs.starfarer.api.impl.campaign.submarkets.MilitarySubmarketPlugin;
 import com.fs.starfarer.api.impl.campaign.submarkets.OpenMarketPlugin;
 import com.fs.starfarer.api.util.Highlights;
+import starpocalypse.config.SimpleMap;
+import starpocalypse.helper.ConfigUtils;
 
 public class RegulatedOpenMarket extends OpenMarketPlugin {
 
@@ -20,16 +28,34 @@ public class RegulatedOpenMarket extends OpenMarketPlugin {
 
     @Override
     public boolean isIllegalOnSubmarket(String commodityId, TransferAction action) {
+        if (isAlwaysLegal(commodityId)) {
+            return false;
+        }
         return plugin.isIllegalOnSubmarket(commodityId, TransferAction.PLAYER_BUY);
     }
 
     @Override
     public boolean isIllegalOnSubmarket(CargoStackAPI stack, TransferAction action) {
+        if (isAlwaysLegal(stack.getDisplayName())) {
+            return false;
+        }
+        if (isStabilityLegal(ConfigUtils.getRegulatedStabilityItem(), stack.getBaseValuePerUnit())) {
+            return false;
+        }
         return plugin.isIllegalOnSubmarket(stack, TransferAction.PLAYER_BUY);
     }
 
     @Override
     public boolean isIllegalOnSubmarket(FleetMemberAPI member, TransferAction action) {
+        if (isCivilian(member.getVariant())) {
+            return false;
+        }
+        if (isAlwaysLegal(member)) {
+            return false;
+        }
+        if (isStabilityLegal(ConfigUtils.getRegulatedStabilityShip(), member.getBaseValue())) {
+            return false;
+        }
         return plugin.isIllegalOnSubmarket(member, TransferAction.PLAYER_BUY);
     }
 
@@ -51,5 +77,62 @@ public class RegulatedOpenMarket extends OpenMarketPlugin {
     @Override
     public Highlights getIllegalTransferTextHighlights(FleetMemberAPI member, TransferAction action) {
         return plugin.getIllegalTransferTextHighlights(member, TransferAction.PLAYER_BUY);
+    }
+
+    @Override
+    public void updateCargoPrePlayerInteraction() {
+        boolean okToUpdate = okToUpdateShipsAndWeapons();
+        super.updateCargoPrePlayerInteraction();
+        if (okToUpdate && ConfigUtils.isLegacyMilitaryRegulations()) {
+            removeItems(submarket.getCargo());
+            removeShips(submarket.getCargo().getMothballedShips());
+        }
+    }
+
+    private boolean isAlwaysLegal(String name) {
+        return ConfigUtils.getRegulatedLegal().has(name);
+    }
+
+    protected boolean isAlwaysLegal(FleetMemberAPI ship) {
+        ShipHullSpecAPI hullSpec = ship.getHullSpec().getBaseHull();
+        if (hullSpec == null) {
+            hullSpec = ship.getHullSpec();
+        }
+        return isAlwaysLegal(hullSpec.getHullName());
+    }
+
+    private boolean isCivilian(ShipVariantAPI variant) {
+        return variant.hasHullMod(HullMods.CIVGRADE) || variant.getHints().contains(ShipTypeHints.CIVILIAN);
+    }
+
+    private boolean isStabilityLegal(SimpleMap stabilityMap, float baseValue) {
+        float stability = submarket.getMarket().getStabilityValue();
+        String stabilityKey = String.valueOf(stability);
+        if (stability <= 0) {
+            return true;
+        }
+        if (stability >= 10) {
+            return false;
+        }
+        float stabilityValue = Float.parseFloat(stabilityMap.get(stabilityKey));
+        return baseValue < stabilityValue;
+    }
+
+    private void removeItems(CargoAPI cargo) {
+        for (CargoStackAPI stack : cargo.getStacksCopy()) {
+            if (isIllegalOnSubmarket(stack, TransferAction.PLAYER_BUY)) {
+                cargo.removeStack(stack);
+            }
+        }
+        cargo.sort();
+    }
+
+    private void removeShips(FleetDataAPI ships) {
+        for (FleetMemberAPI member : ships.getMembersListCopy()) {
+            if (isIllegalOnSubmarket(member, TransferAction.PLAYER_BUY)) {
+                ships.removeFleetMember(member);
+            }
+        }
+        ships.sort();
     }
 }
